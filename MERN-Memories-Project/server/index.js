6488e226531b73159edc49ea8e8b856384631984
Arif Damar <arif660damar@gmail.com>
2021-03-05 14:45:57 +0300

    create backend folder structure and controllers

--- MERN-Memories-Project/server/index.js
+++ MERN-Memories-Project/server/index.js
@@ -3,8 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://arifdamar:8svR2KANabxnDbU@cluster0.wm23p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((err) => console.error(err.message));
+
+mongoose.set('useFindAndModify', false);

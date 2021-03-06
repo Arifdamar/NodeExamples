2cbd59409b5368d95a77ae89600e666f562cfe9a
Arif Damar <arif660damar@gmail.com>
2021-03-06 15:50:03 +0300

    add updating functionality

--- MERN-Memories-Project/client/src/App.js
+++ MERN-Memories-Project/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
@@ -11,10 +11,14 @@ import useStyles from './styles.js';
 const App = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const [currentId, setCurrentId] = useState(null);
+
     useEffect(() => {
         dispatch(getPosts());
     }, [dispatch]);
+
     return (
+
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
@@ -24,10 +28,10 @@ const App = () => {
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts />
+                            <Posts setCurrentId={setCurrentId} />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form />
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
                         </Grid>
                     </Grid>
                 </Container>
--- MERN-Memories-Project/server/controllers/posts.js
+++ MERN-Memories-Project/server/controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 import PostMessage from '../models/PostMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -5,8 +7,7 @@ export const getPosts = async (req, res) => {
         const postMessages = await PostMessage.find();
 
         res.status(200).json(postMessages);
-    }
-    catch (error) {
+    } catch (error) {
         res.status(400).json({ message: error.message });
     }
 }
@@ -22,4 +23,15 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatedPost);
 }
